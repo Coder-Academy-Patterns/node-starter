727afe3a8a321d277ff07ab26ca478703b93408f
Patrick Smith <patrick@burntcaramel.com>
2017-11-01 11:32:34 +1100

    Add intro temperature converter example

---
+++ src/commands/1a-converter.js
@@ -0,0 +1,27 @@
+function convertCelsiusToFahrenheit(celsius) {
+  return (celsius * 1.8) + 32
+}
+
+const mondayTemperature = 24
+console.log(`On Monday it is ${mondayTemperature}°C`)
+
+let fahrenheit = convertCelsiusToFahrenheit(mondayTemperature)
+console.log(`The Americans would think of that as ${fahrenheit}°F`)
+
+
+//
+
+
+const tuesdayTemperature = 31
+console.log(`On Tuesday it is ${tuesdayTemperature}°C`)
+
+fahrenheit = convertCelsiusToFahrenheit(tuesdayTemperature)
+console.log(`The Americans would think of that as ${fahrenheit}°F`)
+
+
+/*
+
+1. Add Fahrenheit to Celsius converter
+
+
+*/
