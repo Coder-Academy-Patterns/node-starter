b7bc199e898c941544c5cb5e3928aaf6780bd6fe
Patrick Smith <patrick@burntcaramel.com>
2017-11-17 11:50:00 +1100

    Add github challenges

--- readme.md
+++ readme.md
@@ -1,13 +1,3 @@
-## Code examples: hard coded
-
-See `src/1-functions` folder.
-
-## Code examples: interactive command line
-
-See `src/2-commands` folder.
-
-
-
 ## Challenges
 
 Challenges can be found within the code (the .js files)
---
+++ src/4-github.js
@@ -0,0 +1,20 @@
+const github = require('../api/github')
+
+github.reposBy('facebook')
+  .then(res => {
+    const repos = res.data
+    console.log(`Found ${repos.length} repos:`)
+    repos.forEach(repo => {
+      console.log(`${repo.name}`)
+    })
+  })
+
+/*
+
+Challenges:
+1. Add usage of the `github.reposBy` to find all repos belonging to 'Microsoft'
+2. Add function to load the info for a particular repo. Add examples to load for repos 'facebook/react' and 'Microsoft/vscode'
+3. Add function to retrieve the contents of the readme file for a particular repo [https://developer.github.com/v3/repos/contents/#get-the-readme]. Hint: see **4-github.rest** file. Add examples to load for repos 'facebook/react' and 'Microsoft/vscode'
+3. Add function to `src/api/github` to list all the issues for a particular repo [https://developer.github.com/v3/issues/#list-issues]. Add examples to load and display key info for repos 'facebook/react' and 'Microsoft/vscode'
+
+*/
