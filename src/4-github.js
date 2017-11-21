a9fa164a4f0aaf755915b2baf857cd55f52de859
Patrick Smith <patrick@burntcaramel.com>
2017-11-22 09:54:10 +1100

    Improve github challenges

--- src/4-github.js
+++ src/4-github.js
@@ -13,8 +13,8 @@ github.reposBy('facebook')
 
 Challenges:
 1. Add usage of the `github.reposBy` to find all repos belonging to 'Microsoft'
-2. Add function to load the info for a particular repo. Add examples to load for repos 'facebook/react' and 'Microsoft/vscode'
-3. Add function to retrieve the contents of the readme file for a particular repo [https://developer.github.com/v3/repos/contents/#get-the-readme]. Hint: see **4-github.rest** file. Add examples to load for repos 'facebook/react' and 'Microsoft/vscode'
-3. Add function to `src/api/github` to list all the issues for a particular repo [https://developer.github.com/v3/issues/#list-issues]. Add examples to load and display key info for repos 'facebook/react' and 'Microsoft/vscode'
+2. Add `infoForRepo(username, repoName)` function to load the info for a particular repo to `src/api/github.js`. Add examples to load for repos 'facebook/react' and 'Microsoft/vscode'. See the .rest file for request
+3. Add `readmeForRepo(username, repoName)` function to retrieve the contents of the readme file for a particular repo [https://developer.github.com/v3/repos/contents/#get-the-readme]. Hint: see **4-github.rest** file. Add examples to load for repos 'facebook/react' and 'Microsoft/vscode'
+3. Add function to `src/api/github.js` to list all the issues for a particular repo [https://developer.github.com/v3/issues/#list-issues]. Add examples to load and display key info for repos 'facebook/react' and 'Microsoft/vscode'
 
 */
